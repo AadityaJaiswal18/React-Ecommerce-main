93a24fcd365340ee8a268bdd81e5a90bf5633adf
Aaditya <jaiswaladitya5904@gmail.com>
2025-11-22 15:06:32 +0530

    Initial commit

---
+++ src/components/main.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <div className="hero border-1 pb-3">
+        <div className="card bg-dark text-white border-0 mx-3">
+          <img
+            className="card-img img-fluid"
+            src="./assets/main.png.jpg"
+            alt="Card"
+            height={500}
+          />
+          <div className="card-img-overlay d-flex align-items-center">
+            <div className="container">
+              <h5 className="card-title fs-1 text fw-lighter">New Season Arrivals</h5>
+              <p className="card-text fs-5 d-none d-sm-block ">
+                This is a premium showcase card with curated details below as a perfect
+                lead-in to your shopping journey. This section offers a little bit
+                more insight.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
